5ea3ebdd4fc26efc9532597de3bafacf86e6e450
Anders Østhus <grapz666@gmail.com>
2013-10-10 20:25:32 +0000

    Now reads events from Azure

--- extractLogs.rb
+++ extractLogs.rb
@@ -12,15 +12,20 @@ tableDate = config.getKey('events.lastReadTable')
 tablePrefix = config.getKey('events.tablePrefix')
 cutOffTime = config.getKey('events.cutOffFromNow')
 
+rollOverTime = 1439 # 23:59
+
 localTime = Time.now
 utcTime = localTime.getutc
-currentDate = localTime.strftime('%Y-%m-%d')
+currentDate = localTime.strftime('%Y%m%d')
 currentTime = utcTime.strftime('%H%M')
 
 partitionKey ||= currentTime
 tableDate ||= currentDate
-tablePrefix ||= "Logs_"
+tablePrefix ||= "Logs"
 table = tablePrefix + tableDate
+
+config.setKey('events.lastReadTable', currentDate)
+config.setKey('events.tablePrefix', tablePrefix)
 
 Azure.configure do |aConfig|
   aConfig.storage_account_name = config.getKey('azure.storage_account_name')
@@ -38,12 +43,35 @@ lastPkToRead = tools.convertToMinutes(currentTime.to_s) - cutOffTime
 #   push events to logstash
 #
 
+azureTableService = Azure::TableService.new
+
 readPk = lastReadPK + 1
 
-(lastReadPK+1..lastPkToRead).each do |pk|
-  query = { :filter => "PartitionKey eq #{pk.to_s}" }
-  puts query
-  readPk = pk.to_s
+if (lastPkToRead < lastReadPK)
+  # We need to roll over to next date
+  (lastReadPK+1..rollOverTime).each do |pk|
+    query = { :filter => "PartitionKey eq #{pk.to_s}" }
+    puts query
+    readPk = pk.to_s
+  end
+
+  (0..lastPkToRead).each do |pk|
+    query = { :filter => "PartitionKey eq #{pk.to_s}" }
+    puts query
+    readPk = pk.to_s
+  end
+else
+  puts "No rollover"
+  (lastReadPK+1..lastPkToRead).each do |pk|
+    query = { :filter => "PartitionKey eq '#{tools.convertToTime(pk)}'" }
+    puts query
+    puts "Table: #{table}"
+    result = azureTableService.query_entities(table, query)
+    result.each do |r|
+      puts "Event found #{r.properties}"
+    end
+    readPk = pk.to_s
+  end
 end
 
-config.setKey('events.lastPK', tools.convertToTime(readPk.to_i))
+#config.setKey('events.lastPK', tools.convertToTime(readPk.to_i))
